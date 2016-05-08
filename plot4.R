ba931521d0940eae4bb70b0506dd92033b296c75
Ceathiel <out.of.bounds@gmail.com>
2016-05-09 01:33:59 +0800

    updating plot 4 to 4 plots in one

--- plot4.R
+++ plot4.R
@@ -23,11 +23,24 @@ plot4 <- function(x="household_power_consumption.txt") {
   datetime <- paste(powerData$Date, powerData$Time)
   powerData$DateTime <- as.POSIXct(datetime, format = "%d/%m/%Y %H:%M:%S")
   
-  # Create plot
-  par(mfrow = c(1, 2), mar = c(4, 4, 1, 1))
+  # Create plots
+  par(mfrow = c(2, 2), mar = c(4, 4, 1, 1))
+  
+  # plot 1
   with(powerData, plot(DateTime, Global_active_power, type="l", xlab = "", ylab = "Global Active Power"))
+  
+  # plot 2
   with(powerData, plot(DateTime, Voltage, type="l", ylab = "Voltage"))
 
+  # plot 3
+  with(powerData, plot(DateTime, Sub_metering_1, type="l", ylab = "Energy sub metering", xlab=""))
+  lines(powerData$DateTime, powerData$Sub_metering_2, type="l", col="red")
+  lines(powerData$DateTime, powerData$Sub_metering_3, type="l", col="blue")
+  legend("topright", pch="-", col=c("black", "blue", "red"), legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), cex=0.8)
+  
+  # plot 4
+  with(powerData, plot(DateTime, Global_reactive_power, type="l"))
+  
   #save it to file
   dev.copy(png,'plot4.png')
   dev.off()
