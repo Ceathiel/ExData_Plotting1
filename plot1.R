719d438e5505a5f2c8fce63dcb40a0eed038c298
Ceathiel <out.of.bounds@gmail.com>
2016-05-09 00:40:14 +0800

    update plot1 to function

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 # Plot 1 - Source code for creating plot 1
 # Please make sure household_power_consumption.txt is in your working directory
-
+plot1 <- function(x="household_power_consumption.txt") {
+  
 # List of packages for session
 message("Loading required packages...")
 pkgList = c("data.table")
@@ -16,15 +17,18 @@ lapply(pkgList, library, character.only=TRUE)
 
 # Read file
 columns <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-powerData <- fread("household_power_consumption.txt", sep = ";", skip="1/2/2007", na.strings = "?", col.names = columns, nrows=2880)
+powerData <- fread(x, sep = ";", skip="1/2/2007", na.strings = "?", col.names = columns, nrows=2880)
 
 # Create histogram
-hist(powerData$Global_active_power, col=c("red"), main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+par(mfrow = c(1, 1))
+hist(powerData$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
 #save it to file
 dev.copy(png,'plot1.png')
 dev.off()
 
 message(paste("Plot saved in ", file.path(getwd(), "plot1.png")))
+
+}
 
 
